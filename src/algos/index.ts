a0fd25beb49f851fdef9343898d0f3be6258b2cb
Ooooiwocha <77987608+Ooooiwocha@users.noreply.github.com>
2025-12-01 23:16:27 +0900

    Update index.ts

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -1,14 +1,18 @@
+// src/algos/index.ts
 import { AppContext } from '../config'
 import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+
+// import * as whatsAlf from './whats-alf'
+import * as gameCreatorsHot from './game-creators-hot'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  // [whatsAlf.shortname]: whatsAlf.handler,
+  [gameCreatorsHot.shortname]: gameCreatorsHot.handler,
 }
 
 export default algos
